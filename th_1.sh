eed99eb2fc803d93bf54ebc206d9624c9b5c4084
Eggy Pandiangam <43237303+eggypandiangan@users.noreply.github.com>
2021-08-23 09:27:44 +0700

    Add files via upload

---
+++ th_1.sh
@@ -0,0 +1,9 @@
+#for i in {01..26}; do
+#	echo $i
+#done
+#for i in {01..26}; do
+for i in {01..13}; do
+	#wget http://s8.bitdl.ir/Series/Trollhunters/S01/Trollhunters.S01E${i}.x265.720p.WEBRip-MRN.Bia2HD.mkv
+	#wget http://s8.bitdl.ir/Series/Trollhunters/S02/Trollhunters.S02E${i}.x265.720p.WEBRip.Bia2HD.mkv
+	wget http://s8.bitdl.ir/Series/Trollhunters/S03/Trollhunters.S03E${i}.x265.720p.WEBRip.Bia2HD.mkv
+done
